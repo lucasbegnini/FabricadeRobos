f7aa3bb3b93e2ab8e75532323d37d586ad78f429
luanmoraesxd <luan_moraes_xs@hotmail.com>
2014-07-29 16:07:12 -0300

    [oq eu fiz man]

---
+++ Luan/AgoraVai - Com Pause/Assets/Scripts/pause.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+	bool paused = false;
+	
+	void Update()
+	{
+	
+
+	}
+
+	void OnMouseDown()
+	{
+		paused = togglePause();
+	}
+
+
+
+	void OnGUI()
+	{
+		if(paused)
+		{
+			GUILayout.Label("Jogo Parado!");
+			if(GUILayout.Button("Click aqui para continuar"))
+				paused = togglePause();
+		}
+	}
+	
+	bool togglePause()
+	{
+		if(Time.timeScale == 0f)
+		{
+			Time.timeScale = 1f;
+			return(false);
+		}
+		else
+		{
+			Time.timeScale = 0f;
+			return(true);   
+		}
+	}
+}
