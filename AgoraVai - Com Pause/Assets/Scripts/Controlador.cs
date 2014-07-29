8e06c4c644e94edbcfd22cbf6140e8ff4a70a878
lucasbegnini <lucasbegnini@gmail.com>
2014-07-29 14:28:56 -0300

    [Att da Tabela e Jogo funcionando com o Botao de Pause]

---
+++ AgoraVai - Com Pause/Assets/Scripts/Controlador.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlador : MonoBehaviour {
+
+	public GameObject sembracoPrefab;
+	private GameObject sembraco;
+
+	public GameObject sempernaPrefab;
+	private GameObject semperna;
+
+	public GameObject semcabecaPrefab;
+	private GameObject semcabeca;
+
+	public GameObject botaobracoPrefab;
+	private GameObject botaobraco;
+
+	public GameObject botaopernaPrefab;
+	private GameObject botaoperna;
+
+	public GameObject botaocabecaPrefab;
+	private GameObject botaocabeca;
+
+	private int robo;
+	private int robosmortos;
+	private int score;
+	private int vida=3;
+	private Vector2 velocidade = new Vector2 (3,0);
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CriarRobos", 2f, 2f); 
+		botaobraco = GameObject.Instantiate(botaobracoPrefab) as GameObject;
+		botaobraco.transform.parent = this.transform;
+
+		botaoperna = GameObject.Instantiate(botaopernaPrefab) as GameObject;
+		botaoperna.transform.parent = this.transform;
+
+		botaocabeca = GameObject.Instantiate(botaocabecaPrefab) as GameObject;
+		botaocabeca.transform.parent = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CriarRobos(){
+		robo = Random.Range (1, 4);
+		switch (robo) {
+		case 1:
+			sembraco = GameObject.Instantiate(sembracoPrefab) as GameObject;
+			sembraco.transform.parent = this.transform;
+			sembraco.rigidbody2D.velocity = velocidade;
+			Debug.Log ("Criou");
+			break;
+			
+		case 2:
+			semperna = GameObject.Instantiate(sempernaPrefab) as GameObject;
+			semperna.transform.parent = this.transform;
+			semperna.rigidbody2D.velocity = velocidade;
+			break;
+			
+		case 3:
+			semcabeca = GameObject.Instantiate(semcabecaPrefab) as GameObject;
+			semcabeca.transform.parent = this.transform;
+			semcabeca.rigidbody2D.velocity = velocidade;
+			break;
+		}
+	}
+
+	void ContarRobosMortos()
+	{
+		robosmortos++;
+		if (robosmortos == 10) 
+		{
+			BroadcastMessage("MaisRapido");
+			robosmortos = 0;
+			velocidade = velocidade + new Vector2 (1.5f,0);
+			sembraco.rigidbody2D.velocity = velocidade;
+		}
+	}
+
+	void CriarBotaoBraco()
+	{
+		botaobraco = GameObject.Instantiate(botaobracoPrefab) as GameObject;
+		botaobraco.transform.parent = this.transform;
+	}
+
+	void CriarBotaoPerna()
+	{
+		botaoperna = GameObject.Instantiate(botaopernaPrefab) as GameObject;
+		botaoperna.transform.parent = this.transform;
+	}
+
+	void CriarBotaoCabeca()
+	{
+		botaocabeca = GameObject.Instantiate(botaocabecaPrefab) as GameObject;
+		botaocabeca.transform.parent = this.transform;
+	}
+
+
+	void Score()
+	{
+		score++;
+		//Debug.Log (score);
+	}
+
+	void MeusPesames()
+	{
+		vida--;
+		//Debug.Log (vida);
+	}
+
+
+
+}
+
---
+++ AgoraVai - Com Pause/Assets/Scripts/botaobraco.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class botaobraco : MonoBehaviour {
+
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+			new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f )); 
+	}
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f); 
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+
+	void OnMouseUp()
+	{
+		SendMessageUpwards ("CriarBotaoBraco");
+		GameObject.Destroy (this.gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D obj)
+	{
+		Debug.Log ("bateu");
+		SendMessageUpwards ("CriarBotaoBraco");
+		GameObject.Destroy (this.gameObject);
+		if (obj.CompareTag ("sembraco")) 
+		{
+			SendMessageUpwards ("Score");
+		}
+	}
+
+
+}
